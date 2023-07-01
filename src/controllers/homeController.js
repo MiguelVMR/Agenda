593a0c11a8f810331c111cbf86d179f06894f43b
MiguelVilela <miguelmvilela12@gmail.com>
2023-07-01 16:32:55 -0300

    Feito read, que busca todos os contatos na home

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,8 @@
+const Contato = require('../models/ContatoModel');
 
-exports.index =  (req,res) =>{ 
-    res.render('index');
+exports.index = async (req,res) =>{ 
+    const contatos = await Contato.buscaContatos();
+    res.render('index',{contatos});
    return;
 }
 
--- src/models/ContatoModel.js
+++ src/models/ContatoModel.js
@@ -17,11 +17,6 @@ function Contato(body) {
     this.errors = [];
     this.contato = null;
 };
-Contato.buscaPorId = async function (id) {
-    if(typeof id !== 'string') return;
-    const user = await ContatoModel.findById(id);
-    return user;
-};
 
 Contato.prototype.register = async function(){
     this.valida();
@@ -61,6 +56,19 @@ Contato.prototype.valida = function() {
     if(this.errors.length > 0) return;
     this.contato = await ContatoModel.findByIdAndUpdate(id,this.body, {new:true});
  }
+
+ // Metodos estáticos
+ Contato.buscaPorId = async function (id) {
+    if(typeof id !== 'string') return;
+    const contato = await ContatoModel.findById(id);
+    return contato;
+};
+
+ Contato.buscaContatos= async function () {
+    const contatos = await ContatoModel.find()
+    .sort({criadoEm: -1});
+    return contatos;
+};
 
 
 module.exports = Contato;
