4d92b1882494f3031780c1d5efcfca3e599f6cb6
MiguelVilela <miguelmvilela12@gmail.com>
2023-07-01 10:40:55 -0300

    Validação de login feita

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -28,3 +28,29 @@ exports.register = async function(req,res) {
     }
     
 };
+
+exports.login = async function(req,res) {
+    try{
+    const login = new Login(req.body);
+    await login.login();
+    
+    if(login.errors.length > 0 ) {
+        req.flash('errors',login.errors);
+        req.session.save(function() {
+           return res.redirect('/login/index');
+        });
+        return;
+    }
+
+    req.flash('success','Você entrou no sistema.');
+    req.session.user = login.user;
+    req.session.save(function() {
+       return res.redirect('/login/index');
+    });
+
+    } catch(e){
+        console.log(e);
+       return res.render('404');
+    }
+    
+};
--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -17,6 +17,25 @@ class Login {
     this.errors = [];
     this.user = null;
  }
+
+ async login() {
+   this.valida();
+   if(this.errors.length > 0) return;
+   this.user = await LoginModel.findOne({email: this.body.email});
+
+   if(!this.user) {
+      this.errors.push('Usuário não existe.');
+      return;
+   }
+
+   if(!bcryptjs.compareSync(this.body.password, this.user.password)){
+      this.errors.push('Senha incorreta.');
+      this.user = null;
+      return;
+   }
+ }
+
+
  async register() {
     this.valida();
     if(this.errors.length > 0) return;
@@ -28,15 +47,13 @@ class Login {
     const salt = bcryptjs.genSaltSync();
     this.body.password = bcryptjs.hashSync(this.body.password,salt);
    
-    try{
-        this.user = await LoginModel.create(this.body);
-    } catch (e){
-        console.log(e);
-    }
+
+      this.user = await LoginModel.create(this.body);
  }
+
  async userExists() {
-    const user = await LoginModel.findOne({email: this.body.email});
-    if (user) this.errors.push('Usuário já existe.');
+    this.user = await LoginModel.findOne({email: this.body.email});
+    if (this.user) this.errors.push('Usuário já existe.');
  }
 
  valida() {
